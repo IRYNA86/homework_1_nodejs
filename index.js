e4d8a14b3514d273babf7becd8548eae83d46207
IRYNA86 <88017665+IRYNA86@users.noreply.github.com>
2022-08-16 00:39:30 +0300

    done

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const contacts = require("./contacts");
 
 const program = new Command();
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+.option("-a, --action <type>", "choose action")
+.option("-i, --id <type>", "user id")
+.option("-n, --name <type>", "user name")
+.option("-e, --email <type>", "user email")
+.option("-p, --phone <type>", "user phone");
   
 program.parse(process.argv);
 
