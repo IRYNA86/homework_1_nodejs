9a544df3375893ba041818962c01d14a28d37d11
IRYNA86 <88017665+IRYNA86@users.noreply.github.com>
2022-08-16 00:35:33 +0300

    done

---
+++ README.md
@@ -0,0 +1,15 @@
+listContacts:
+
+https://monosnap.com/file/RoINavABYKIA1D8Oq0lnFJqAUhv3RQ
+
+getContactById:
+
+https://monosnap.com/file/ju3iBccMdeGKtnKzzs7JpYXrTrUWGm
+
+addContact:
+
+https://monosnap.com/file/HlV0IB7Q2QGK48esxSb9nylias2JVW
+
+removeContact:
+
+https://monosnap.com/file/etZVyh0i1FTvuGou0zg4GnXz9Rot5D
--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-const contacts = require('./contacts');
+const { Command } = require("commander");
+const contacts = require("./contacts");
 
-const invokeAction = async ({action, id, name, email, phone}) => {
-    switch(action){
-        case 'list':
-            const allContacts = await contacts.listContacts();
-            console.table(allContacts);
-            break;
-            case 'get':
-                const oneContact = await contacts.getContactById(id);
-                console.log(oneContact);
-                break;
-            default:
-                console.log('Unknown action');
-    }
-}
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+  
+program.parse(process.argv);
 
-invokeAction({action: 'list'})
-invokeAction({action: 'get', id: '3'})
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    case "remove":
+      const removeContactById = await contacts.removeContact(id);
+      console.log(removeContactById);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
